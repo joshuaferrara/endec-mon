365ce581a06ddcbdf9f96153f4f8d5ab3f21619c
Joshua Ferrara <scjosh2@gmail.com>
2015-09-19 04:54:30 +0000

    Added code

---
+++ monitor.js
@@ -0,0 +1,28 @@
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+var request = require("request");
+var fs = require("fs");
+var config = require("./config");
+
+var serialPort = new SerialPort("/dev/ttyUSB0", {
+	baudrate: 9600,
+	parser: serialport.parsers.readline("<ENDECEND>"),
+});
+
+serialPort.on("open", function() {
+	console.log("ENDEC serial connection opened!");
+
+	serialPort.on("data", function(data) {
+		console.log("Got data from endec: " + data);
+		request.post(config.iftttURL, function(err, resp, body) {
+			console.log(body);
+		});
+		fs.appendFile("alerts.txt", data, function(err) {
+			if (err)
+				console.error(err);
+		});
+		fs.appendFile("alerts.txt", "<ENDECEND>", function(err) {
+
+		})
+	});
+});
